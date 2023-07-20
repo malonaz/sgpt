a6d494feb14901173c49bff877d99aad446d3aa5
Malon Azria <malonazria@gmail.com>
2023-07-20 23:16:04 +0200

    [diff] - refactor: improve prompt and example text formatting
     - Replace triple backticks with placeholders to avoid markdown issues.
     - Add more examples and clearer formatting guidelines in prompt text.

--- diff/diff.go
+++ diff/diff.go
@@ -21,14 +21,44 @@ import (
 )
 
 const prompt = `IMPORTANT: Provide only plain text without Markdown formatting.
-IMPORTANT: Do not include markdown formatting such as ` + "```" + `.
+IMPORTANT: Do not include markdown formatting such as "@@@".
 Output a git commit messages for the provided diff using the following format:
-Short (72 chars or less) summary
+@@@
+[{scope}] - {type}: {summary}
 
-- Bullet points are okay, too.
-- Typically a hyphen or asterisk is used for the bullet, followed by a
-  single space. Use a hanging indent.
+ - {bullet_point}
+ - {bullet_point}
+@@@
+
+Documentation:
+@@@
+ summary: A 50 character summary. should be present tense. Not capitalized. No period in the end.”, and imperative like the type.
+ scope: The package or module that is affected by the change. This field is optional, only include it if the changes particularly target a single area. If the no particular area can be targeted, use "misc".
+ type: One of "fix, feature, refactor, test, devops, docs". Indicates the type of change being done.
+ bullet_point: An sentence explaining why we’re changing the code, compared to what it was before.
+@@@
+
+Examples:
+@@@
+[reporting] - feature: add automatic generation of PnL reports for competitors
+
+ - Every 24 hours, a job is triggered to generate the PnL reports of all competitors and upload them to an S3 bucket.
+ - Failed jobs are retried with an exponential backoff.
+@@@
+@@@
+[trading] - refactor: remove @gas_limit@ field from @Calldata@ protobuf
+
+ - @gas_limit@ has been replaced by @gas_price@ and all clients have stopped using it.
+@@@
+@@@
+[price_model] - test: cover case where Binance price feed disconnects
+@@@
+@@@
+[env] - devops: add ClusterRoleBinding between price-model ServiceAccount and grpc-client-kube-resolver ClusterRole
+@@@
+
 `
+
 
 const generateGitCommitMessage = `Generate a git commit message.
 Think step-by-step to ensure you only write about meaningful high-level changes.
@@ -47,7 +77,7 @@ func NewCmd(openAIClient *openai.Client, config *configuration.Config) *cobra.Co
 		Model   *model.Opts
 		Message string
 	}
-
+	prompt := strings.ReplaceAll(prompt, "@", "`")
 	cmd := &cobra.Command{
 		Use:   "diff",
 		Short: "Generate diff commit message",
