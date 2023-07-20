f9fc502a2253b11b60084da9e41497bed3a717b9
Malon Azria <malonazria@gmail.com>
2023-07-20 23:20:51 +0200

    [diff] - refactor: update scope documentation and bullet_point format
     - Clarify scope definition in documentation to better identify affected areas
     - Remove periods from bullet_point sentences for consistency

--- diff/diff.go
+++ diff/diff.go
@@ -33,7 +33,8 @@ Output a git commit messages for the provided diff using the following format:
 Documentation:
 @@@
  summary: A 50 character summary. should be present tense. Not capitalized. No period in the end.”, and imperative like the type.
- scope: The package or module that is affected by the change. This field is optional, only include it if the changes particularly target a single area. If the no particular area can be targeted, use "misc".
+ scope: The package or module that is affected by the change. This field is optional, only include it if the changes particularly target a single area.
+        If the no particular area can be targeted, use "misc". If most of the changes happen in ./folder_a/, then the scope would be @folder_a@
  type: One of "fix, feature, refactor, test, devops, docs". Indicates the type of change being done.
  bullet_point: An sentence explaining why we’re changing the code, compared to what it was before.
 @@@
@@ -42,13 +43,13 @@ Examples:
 @@@
 [reporting] - feature: add automatic generation of PnL reports for competitors
 
- - Every 24 hours, a job is triggered to generate the PnL reports of all competitors and upload them to an S3 bucket.
- - Failed jobs are retried with an exponential backoff.
+ - Every 24 hours, a job is triggered to generate the PnL reports of all competitors and upload them to an S3 bucket
+ - Failed jobs are retried with an exponential backoff
 @@@
 @@@
 [trading] - refactor: remove @gas_limit@ field from @Calldata@ protobuf
 
- - @gas_limit@ has been replaced by @gas_price@ and all clients have stopped using it.
+ - @gas_limit@ has been replaced by @gas_price@ and all clients have stopped using it
 @@@
 @@@
 [price_model] - test: cover case where Binance price feed disconnects
