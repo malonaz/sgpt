9d595b1f988a96ae15f0f8dab1dce56d13f37517
Malon <malonazria@gmail.com>
2026-05-03 13:30:41 -0400

    remove unused import

--- internal/toolengine/toolengine.go
+++ internal/toolengine/toolengine.go
@@ -23,7 +23,6 @@ import (
 
 	sgptpb "github.com/malonaz/sgpt/genproto/sgpt/v1"
 	"github.com/malonaz/sgpt/internal/cache"
-	"github.com/malonaz/sgpt/internal/debug"
 	"github.com/malonaz/sgpt/internal/tools"
 )
 
